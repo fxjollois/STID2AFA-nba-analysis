d97d99ec1abfa755a5ab5985c45fda8a4fccb519
FX Jollois <fxjollois@gmail.com>
2015-04-20 09:16:30 +0200

    Script séance du 14 avril

---
+++ TP2.md
@@ -0,0 +1,6 @@
+# TP2
+
+Faire un fichier HTML qui permettra :
+- le choix d'une date (à mettre de côté pour le moment, on travaillera sur le 13 avril)
+- le choix de ce qu'on veut voir afficher (et donc un menu avec la date + les matchs)
+- la présentation de ce qu'on a choisi (entre la soirée ou un match particulier)
---
+++ script.js
@@ -0,0 +1,63 @@
+function recupDate() {
+	var date = document.getElementById("inpDate").value;
+	var jour = date.substring(0, 2);
+	var mois = date.substring(3, 5);
+	var annee = date.substring(6, 10);
+	if (date === "" | isNaN(jour) | jour==="" | isNaN(mois) | mois==="" | isNaN(annee) | annee==="") {
+		var hier = new Date(new Date().getTime()-(24*60*60*1000));
+		var jour = ('0'+hier.getDate()).slice(-2);
+		var mois = ('0'+(hier.getMonth()+1)).slice(-2);
+		var annee = hier.getFullYear();
+		document.getElementById("inpDate").value = jour + "/" + mois + "/" + annee ;
+	}
+	date = annee + mois + jour;
+	return (date);
+}
+
+function infoMatch (dirMatch) {
+	$.getJSON('games/'+dirMatch+'/boxscore.json',
+		function(data) {
+			var v = data.sports_content.game.visitor;
+			var h = data.sports_content.game.home;
+			$('#contenu').html(v.city + ' ' + v.nickname + ' (' + v.score + ') @ ' + h.city + ' ' + h.nickname + ' (' + h.score + ')');
+		});
+}
+
+function chargeDate() {
+	// fonction lancé lorsque la page est chargée
+	var date = recupDate();
+	$('nav:first').html('');
+	$('#contenu').html('');
+	$.getJSON('games/'+date+'/games.json',
+		function(data) {
+			// on va faire un menu
+			$('nav:first')
+				.append($(document.createElement('div'))
+							.addClass("date")
+							.click(function() { chargeDate() })
+							.html($('#inpDate').val())
+						)
+				.append($(document.createElement('ul')));
+			$.each(data.sports_content.games.game, 
+				function(i, m) {
+					$('nav:first ul')
+						.append($(document.createElement('li'))
+									.click(function() { infoMatch(date + '/' + m.id); })
+									.html(m.visitor.nickname+'@'+m.home.nickname)
+						)
+					});
+
+			$('#contenu')
+				.append($(document.createElement('div'))
+							.addClass("resume")
+							.append($(document.createElement('h2')).html('Résumé de la soirée'))
+							.append($(document.createElement('p'))
+										.html(data.sports_content.games.game.length + ' matchs, ' + 
+												$.map(data.sports_content.games.game, 
+														function(m) { return (parseInt(m.visitor.score)+parseInt(m.home.score)) })
+													.reduce(function(a, b) { return (a+b) }) + ' points marqués')
+									)
+						);
+		})
+		.fail(function(e) { $('#contenu').html("Il n'y a pas de match pour la date indiquée."); });
+}
